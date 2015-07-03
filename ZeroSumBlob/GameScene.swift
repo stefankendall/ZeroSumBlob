f3e739153307cfcf361c683d91a00a7d4599fbf7
Stefan Kendall <stefankendall@gmail.com>
2015-07-03 17:21:06 -0400

    Blob can move

--- ZeroSumBlob/GameScene.swift
+++ ZeroSumBlob/GameScene.swift
@@ -6,12 +6,30 @@ class GameScene: SKScene {
 
         let background = SKSpriteNode(imageNamed: "background.png")
         background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        background.name = "background"
         self.addChild(background)
 
-        let blob = BlobNode(name:"me", color: UIKit.UIColor.greenColor())
+        let blob = BlobNode(color: UIKit.UIColor.greenColor(), playerName: "mr. blob")
+        blob.name = "me"
         background.addChild(blob)
+
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
     }
 
     override func update(currentTime: CFTimeInterval) {
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.moveToward(touches.first!)
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.moveToward(touches.first!)
+    }
+
+    func moveToward(touch: UITouch) {
+        let blob: BlobNode = self.childNodeWithName("//me") as! BlobNode
+        let touchPoint: CGPoint = touch.locationInNode(self.childNodeWithName("background")!)
+        blob.moveTowardPoint(touchPoint)
     }
 }
--- ZeroSumBlob/Nodes/BlobNode.swift
+++ ZeroSumBlob/Nodes/BlobNode.swift
@@ -3,18 +3,32 @@ import SpriteKit
 import UIKit
 
 class BlobNode: SKNode {
-    init(name: String, color blobColor: UIColor) {
+    var blobRadius: CGFloat
+
+    init(color blobColor: UIColor, playerName: String) {
+        self.blobRadius = 15
         super.init()
 
-        self.name = name
-        let body: SKShapeNode = SKShapeNode(circleOfRadius: 15);
+        self.physicsBody = SKPhysicsBody(circleOfRadius: self.blobRadius)
+
+        let body: SKShapeNode = SKShapeNode(circleOfRadius: self.blobRadius);
         body.fillColor = blobColor
         body.lineWidth = 3;
         body.strokeColor = blobColor.lighterColorWithBrightnessFactor(-0.2)
         self.addChild(body)
+
+        let playerNameLabel = SKLabelNode(text: playerName)
+        playerNameLabel.fontName = "AvenirNext-Bold"
+        playerNameLabel.fontSize = 12
+        playerNameLabel.verticalAlignmentMode = SKLabelVerticalAlignmentMode.Center
+        self.addChild(playerNameLabel)
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("not implemented")
+    }
+
+    func moveTowardPoint(point: CGPoint) {
+        self.physicsBody!.velocity = CGVector(dx: point.x - self.position.x, dy: point.y - self.position.y)
     }
 }
